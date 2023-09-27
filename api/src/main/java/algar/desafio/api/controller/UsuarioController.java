37a42386cc3e4159f1c089ba1e1f7922dae75c4e
igorcl2000 <igorcl2000@gmail.com>
2023-09-26 22:22:26 -0300

    Update UsuarioController ResponseEntity
    Update UsuarioController ResponseEntity

--- api/src/main/java/algar/desafio/api/controller/UsuarioController.java
+++ api/src/main/java/algar/desafio/api/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import algar.desafio.api.usuario.DadosAtualizacaoUsuario;
 import algar.desafio.api.usuario.DadosCadastroUsuario;
@@ -32,36 +34,49 @@ public class UsuarioController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroUsuario dados) {
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroUsuario dados, UriComponentsBuilder uriBuilder) {
         System.out.println("dados recebidos: " + dados);
-        repository.save(new Usuario(dados));
+
+        var usuario = new Usuario(dados);
+
+        repository.save(usuario);
+
+        var uri = uriBuilder.path("/usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosListagemUsuario(usuario));
     }
 
     @GetMapping
-    public Page<DadosListagemUsuario> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemUsuario::new);
+    public ResponseEntity<Page<DadosListagemUsuario>> listar(
+            @PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemUsuario::new);
 
+        return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    public DadosListagemUsuario listar(@PathVariable Long id) {
+    public ResponseEntity listar(@PathVariable Long id) {
         var usuario = repository.getReferenceById(id);
-        return new DadosListagemUsuario(usuario);
+        return ResponseEntity.ok(new DadosListagemUsuario(usuario));
 
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoUsuario dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoUsuario dados) {
         var usuario = repository.getReferenceById(dados.id());
         usuario.DadosAtualizacaoUsuario(dados);
+
+        return ResponseEntity.ok(new DadosListagemUsuario(usuario));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void remover(@PathVariable Long id) {
+    public ResponseEntity remover(@PathVariable Long id) {
         var usuario = repository.getReferenceById(id);
         usuario.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 
 }
